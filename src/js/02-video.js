0d9e3214a9b6e31da49326b94e5dde47b9ba7706
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-07-27 21:19:23 +0200

    początek zadanie 2 i 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on("timeupdate", ({ duration, percent, seconds }) => {
+      throttle(() => { localStorage.setItem("videoplayer-current-time", `${seconds}`); }, 1000);
+      
+})
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
 
+
+const form = document.querySelector("form.feedback-form");
+const input = document.querySelector("input");
+const textArea = document.querySelector("textarea");
+
+form.addEventListener("input", (event) => {
+      const { elements: { email, message } } = event.currentTarget;
+      const objectInput = { email: email.value, message: message.value };
+      localStorage.setItem("feedback-form-state", JSON.stringify(objectInput));
+      
+})
+
+form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const { elements: { email, message } } = event.currentTarget;
+      console.log({ email: email.value, message: message.value });
+      
+      event.currentTarget.reset();
+      localStorage.clear();
+});
+
+const storageData = localStorage.getItem("feedback-form-state");
+const parseStorageData = JSON.parse(storageData);
+const tryFoo = () => {
+      if (parseStorageData.email !== "" || parseStorageData.message !== "") {
+            input.value = parseStorageData.email;
+            textArea.value = parseStorageData.message;
+      };
+};
+tryFoo();
+console.log(parseStorageData);
