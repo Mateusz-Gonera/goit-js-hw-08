b926519a36475b5074154966a1c4adb568b35a8f
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-07-28 11:21:17 +0200

    Zadanie 2 bez throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,6 +5,23 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on("timeupdate", ({ duration, percent, seconds }) => {
-      throttle(() => { localStorage.setItem("videoplayer-current-time", `${seconds}`); }, 1000);
-      
-})
+      localStorage.setItem("videoplayer-current-time", `${seconds}`);
+}) 
+
+const actualTime = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(actualTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+console.log(actualTime);
